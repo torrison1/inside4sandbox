7bef92051bba67c4fc131f1c7fee0d744f344ce5
Torrison <torrison1@gmail.com>
2020-04-27 03:35:36 +0300

    U

--- AppControllers/Inside.php
+++ AppControllers/Inside.php
@@ -11,6 +11,13 @@ Class Inside extends BaseController
 
     }
 
+    public function modules_from_xml() {
+        // echo __DIR__;
+
+        $xml_string = file_get_contents(__DIR__.'/../xTMP/modules.xml');
+        $xml = simplexml_load_string($xml_string);
+        $this->response->echo_json($xml);
+    }
     public function modules() {
 
         // Modules : Files - Blocks, Tables Columns, Dependencies - Modules, Links - Interfaces, API Docs, Documents, Help Info Links
@@ -18,33 +25,36 @@ Class Inside extends BaseController
         // TO DO
         $res['modules'] = Array(
             Array(
-                'id' => 1, 'name' => 'Inside Enter Point : index.php',
+                'id' => 1,
+                'name' => 'Inside Enter Point : index.php',
                 'files' => Array(
-                    Array('path' => 'index.php', 'info' => 'Enter Point',
+                    Array(
+                        'path' => 'index.php',
+                        'info' => 'Enter Point',
 
                         'blocks' => Array(
                             Array(
                                 'block_id' => '1',
                                 'block_name' => 'Show ALL Errors',
                                 'block_code' => 'error_reporting( E_ALL );',
+                                'block_info' => 'Can be set in php.ini',
                                 'advanced' => 'Need to Comment in PROD Mode',
                             )
                         )
-
-                        )
+                    )
                 )
             ),
             Array(
-                'id' => 1, 'name' => 'Inside Default Files',
+                'id' => 2, 'name' => 'Inside Default Files',
                 'files' => Array(
                     Array('path' => 'config.php', 'info' => 'Main Config file. if it not exists, you need to copy from config_default.php'),
                     Array('path' => 'config_default.php', 'info' => 'Default Config file example. if config.php is not exists, you need to copy from config_default.php'),
 
                 )
             ),
-            Array('id' => 2, 'name' => 'Inside Globals Class'),
-            Array('id' => 3, 'name' => 'Inside Routing Class'),
-            Array('id' => 4, 'name' => 'Inside Base Controller Class'),
+            Array('id' => 3, 'name' => 'Inside Globals Class'),
+            Array('id' => 4, 'name' => 'Inside Routing Class'),
+            Array('id' => 5, 'name' => 'Inside Base Controller Class'),
 
         );
 
